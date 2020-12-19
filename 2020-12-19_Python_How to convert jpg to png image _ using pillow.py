26b6232feb81ba4b7e934f19c2e2e8ade3cf8897
erniejie <46618858+Erniejie@users.noreply.github.com>
2020-12-19 18:10:42 +0000

    Add files via upload

---
+++ 2020-12-19_Python_How to convert jpg to png image _ using pillow.py
@@ -0,0 +1,15 @@
+#How to convert jpg to png image using python || pillow python- I know Python YT
+
+from PIL import Image
+
+#EXAMPLE 1: From jpg(JPEG) to png
+
+image=Image.open("yijiefoodsample.jpg")
+
+# Converting jpg to png image:
+
+image.save("yijiefoodsample_converted.png")
+
+
+#EXAMPLE 2: from png to jpg
+
